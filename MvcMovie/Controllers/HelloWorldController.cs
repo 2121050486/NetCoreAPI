0a84e4d05bbb22bee4e5ee51590341a80f9e6a7b
2121050486 <2121050486@student.humg.edu.vn>
2024-09-18 19:45:26 +0700

    update_Code

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -1,7 +1,19 @@
-namespace Net_Core_API;
-
-public class HelloWorldController 
-
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+namespace MvcMovie.Controllers
 {
-    
+    public class HelloWorldController : Controller
+    { 
+        // GET: /HelloWorld/
+        public IactionResult Index()
+        {
+            return view();
+        } 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
 }
